b8dd5536f71f6ed4f24b3da90a0173d98354d379
lcisme <cuongpubg.7@gmail.com>
2022-03-02 20:19:51 +0700

    cmsn

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Ha Linh';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100056048947923';
+const eventName = 'Happy Birthday';
+const titleCard = 'Ha Linh';
+const contentCard = 'Chúc Leader Linh có giây phút hạnh phúc và vui vẻ bên người thân và bạn bè. Hi vọng Linh sẽ thành công trong cuộc sống';
+
+const giftImage = 'dhl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
---
+++ present.js
@@ -0,0 +1,52 @@
+
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+
+var to = nameGirl;
+var gift_url = giftUrl;
+var gift_image_url = giftImage || giftImageBase64;
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+function init() {
+  var graphElem = document.querySelector('.present-box > .side.top .to');
+  graphElem.setAttribute('data-before', eventName);
+  document.querySelector('#card .title-card').innerHTML = `🎂${titleCard}🧁`;
+  document.querySelector('#card .content-card').innerHTML = `${contentCard}`;
+  document.querySelector('#card .honey').setAttribute('src', `${giftImage}`);
+
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
